33770ae43ca05e7e4a4646eb400bdac686f3e557
Martino Thomann <martino.thomann@spreadgroup.com>
2023-10-28 10:45:50 +0200

    updated graph generation script

--- scripts/generate_graphs.py
+++ scripts/generate_graphs.py
@@ -9,14 +9,14 @@ scenarios_desc = ["Identisch", "Skaliert", "Verschoben", "Hintergrund", "Rotiert
                   "Gemischt"]
 
 for algorithm in algorithms:
-    algorithm_file_name = algorithm
+    matcher = ""
     if algorithm in ["sift", "orb", "brisk"]:
-        algorithm_file_name += "-bfm"
+        matcher += "-bfm"
 
     scenario_index = 0
     for scenario in scenarios:
 
-        csv = pd.read_csv(f"../test-output/csv-files/{scenario}/{algorithm_file_name}-overall-evaluation.csv")
+        csv = pd.read_csv(f"../test-output/csv-files/{algorithm}/{scenario}{matcher}-overall-evaluation.csv")
         csv = csv.sort_values(by="threshold")
 
         threshold_values = csv["threshold"]
@@ -28,8 +28,8 @@ for algorithm in algorithms:
         scenario_index += 1
 
         plt.plot(threshold_values, recall_values, label="Recall")
-        plt.plot(threshold_values, specificity_values, label="Specificity")
-        plt.plot(threshold_values, accuracy_values, label="Balanced Accuracy", linestyle="--")
+        plt.plot(threshold_values, specificity_values, label="Spezifität")
+        plt.plot(threshold_values, accuracy_values, label="Balancierte-Genauigkeit", linestyle="--")
 
         plt.ylim(-0.05, 1.12)
 
@@ -39,36 +39,38 @@ for algorithm in algorithms:
         else:
             plt.xlabel("Grenzwerte Bildähnlichkeits-Score")
 
-        plt.ylabel("Zuverlässigkeits Bewertung")
+        plt.ylabel("Zuverlässigkeitsbewertung")
         plt.legend(loc='upper left', ncol=3, frameon=False)
 
         plt.savefig(f'../test-output/graphs/{algorithm}/{scenario}.png')
 
         plt.clf()
 
-# recall_list = []
-# specificity_list = []
-# accuracy_list = []
-#
-# for scenario in scenarios:
-#     csv = pd.read_csv(f"../test-output/csv-files/{scenario}/phash-overall-evaluation.csv")
-#     csv = csv.sort_values(by="threshold")
-#
-#     standard_phash_evaluation = csv[csv["threshold"] == 4]
-#     recall_list.append(standard_phash_evaluation["recall"][0])
-#     specificity_list.append(standard_phash_evaluation["specificity"][0])
-#     accuracy_list.append(standard_phash_evaluation["balanced accuracy"][0])
-#
-# bar_width = 0.25
-# bar_positions = np.arange(len(scenarios))
-#
-# plt.bar(bar_positions, recall_list, bar_width, label='Recall')
-# plt.bar(bar_positions, specificity_list, bar_width, label='Specificity')
-# plt.bar(bar_positions, accuracy_list, bar_width, label='Balanced Accuracy')
-#
-#
-# plt.xticks(bar_positions, scenarios_desc, rotation=45)
-# plt.legend(loc='upper right', bbox_to_anchor=(1, 1.3))
-# plt.subplots_adjust(bottom=0.175, top=0.8)
-#
-# plt.savefig(f'../test-output/graphs/phash/standard.png', )
+recall_list = []
+specificity_list = []
+accuracy_list = []
+
+for scenario in scenarios:
+    csv = pd.read_csv(f"../test-output/csv-files/phash/{scenario}-overall-evaluation.csv", index_col=0)
+    csv = csv.sort_values(by="threshold")
+
+    standard_phash_evaluation = csv.loc[4]
+    recall_list.append(standard_phash_evaluation["recall"])
+    specificity_list.append(standard_phash_evaluation["specificity"])
+    accuracy_list.append(standard_phash_evaluation["balanced accuracy"])
+
+bar_width = 0.25
+bar_positions = np.arange(len(scenarios))
+
+plt.bar(bar_positions-bar_width, recall_list, bar_width, label='Recall')
+plt.bar(bar_positions, specificity_list, bar_width, label='Spezifität')
+plt.bar(bar_positions+bar_width, accuracy_list, bar_width, label='Balancierte-Genauigkeit')
+
+plt.ylim(-0.05, 1.12)
+plt.ylabel("Zuverlässigkeitsbewertung")
+plt.xticks(bar_positions, scenarios_desc, rotation=45)
+plt.legend(loc='upper left', ncol=3, frameon=False)
+
+plt.subplots_adjust(bottom=0.175, top=0.8)
+
+plt.savefig(f'../test-output/graphs/phash/standard.png')
